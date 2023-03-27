75257731ed2651bb58d981f678465d563c4064be
Nayelli Olvera <nattkeres@gmail.com>
2023-03-26 22:45:31 -0600

    exercise 1

--- 01_contador_incremental/ejercicio/js/index.js
+++ 01_contador_incremental/ejercicio/js/index.js
@@ -1,3 +1,14 @@
 
 // Escribir una función clousure que 
 // aumente un contador en 1 cada vez que la función se llame
+function counter(num) {
+    return function () {
+        return num++
+    }
+}
+
+const count = counter(1);
+
+console.log(count());
+console.log(count());
+console.log(count());
