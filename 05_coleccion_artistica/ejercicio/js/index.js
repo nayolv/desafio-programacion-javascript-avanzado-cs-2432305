34fb4deed6141dcbd1d7d43eee6bb3d90f13868e
Nayelli Olvera <nattkeres@gmail.com>
2023-03-28 17:36:39 -0600

    exercise 5

--- 05_coleccion_artistica/ejercicio/js/index.js
+++ 05_coleccion_artistica/ejercicio/js/index.js
@@ -1,6 +1,16 @@
-
 // Cargar las imágenes contenidas en la constante rutasImagenes utilizando Promise.all
 // Luego cargadas, agregarlas al div #contenedor
 
 const rutasImagenes = imagenes;
-const contenedor = document.getElementById('contenedor');
+const contenedor = document.getElementById("contenedor");
+
+Promise.all(rutasImagenes)
+    .then((arr) =>
+        arr.forEach(img => {
+            const images = document.createElement("img");
+            images.src = img.src;
+            contenedor.appendChild(images);
+  }
+  
+  ))
+  .catch((err) => console.log(err));
